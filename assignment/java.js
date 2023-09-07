94c2c31e9287008ce7195e28d27775cffe55f504
sidra mursaleen <sidramursaleen333@gmail.com>
2023-09-07 13:33:33 +0500

    work complete

---
+++ assignment/java.js
@@ -0,0 +1,660 @@
+// chapter no 1
+
+// q no 1
+
+// alert("Sidra");
+
+// alert("Mursaleen");
+
+// alert("sidramursaleen333@gmail.com");
+
+// alert("000-000000-00");
+
+// alert(".............");
+
+// q no 2
+
+// alert("You're learning JavaScript!");
+
+// q no 3
+
+// alert("Welcome! To our website");
+
+// chapter no 2
+
+// q no 1
+
+// var firstName ;
+
+// q no 2
+
+// var name ;
+
+// name = "sidra" ;
+
+// q no 3
+
+// var teamName = "jawan pakistan" ;
+
+// alert(teamName);
+
+// var betsMen = "Babar Azam" ;
+
+// chapter no 3
+
+// q no 1
+
+// var caseQty ;
+
+// q no 2
+
+// caseQty = 144 ;
+
+// q no 3
+
+// var num = 9 ;
+
+// q no 4
+
+// var sum ;
+
+// sum = 10 + 7 ;
+
+// q no 5
+
+// var merchTotal = 100 , shippingCharges = 10 , orderTotal = merchTotal + shippingCharges ;
+
+// document.write(orderTotal);
+
+// q no 6
+
+// var num = 2 ;
+
+// num = num + 2 ;
+
+// chapter no 4
+
+// q no 1
+
+// var productCost = 3.45 ;
+
+// q no 2
+
+// var nameOfBand ;
+
+// q no 3
+
+// var num = 2 ;
+
+// q no 4
+
+// var sidraMursaleen ;
+
+// q  no 5
+
+// 5 legal variable
+
+// var firstName , last_Name , name2 , age , qualification ;
+
+// 5 illegal varaible
+
+// var 2name , first name , #age , alert , last+name ;
+
+// chapter no 5
+
+// q no 1
+
+// The name and symbol of arithmetic operator that gives you remainder is modulus( % ) .
+
+// q no 2
+
+// var num = 20 % 6 ;
+
+// document.write(num);
+
+// q no 3
+
+// var largeNum = 1,000 * 2,000 ;
+
+// q no 4
+
+// var num1 = 10 , num2 = 7 , subtraction = num1 - num2 ;
+
+// q no 5
+
+// var operation = 20 % 6 ; 
+
+// q no 6
+
+// var multiplication = 3 * 5 ;
+
+// alert(multiplication);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ class work/java.js
@@ -0,0 +1,769 @@
+// chater no 1
+
+// q no 1
+
+// Swal.fire(
+//   'Welcome! To our website .',
+//   'Hope you are fine!',
+//   'success'
+// )
+
+// q no 2
+
+// alert("Error! Please enter a valid passward");
+
+// q no 3
+
+// alert("Welcome to js land... \n Happy Coding");
+
+// q no 4
+
+// alert("Welcome to js land");
+
+// alert("Happy coding \n Prevent this page from creating additional dialogs");
+
+// q no 5
+
+// console.log("Hello... I can run js through my web browser's console")
+
+// q no 6
+
+// question no 6 and 7 are done
+
+// chapter no 2
+
+// q no 1
+
+// var userName;
+
+// q no 2
+
+// var myName = "sidra mursaleen";
+
+// q no 3
+
+// var message = "Hello World";
+
+// alert(message);
+
+// q no 4
+
+// var name = "Jhon Doe" , age = "15 years old" , certified = "Certified mobile app development";
+
+// alert(name);
+
+// alert(age);
+
+// alert(certified);
+
+// q no 5
+
+// var food = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+
+// alert(food);
+
+// q no 6
+
+// var email = "example@example.com";
+
+// alert("my email address is" + " " + email);
+
+// q no 7
+
+// var book = "a samrter way to learn java script";
+
+// alert("I am trying to learn from the book" + " " + book);
+
+// q no 8
+
+// document.write("Yah! I can write HTML content through java script");
+
+// q no 9
+
+// var string = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+
+// alert(string);
+
+// chapter no 3
+
+// q no 1
+
+// var age = "15 years old";
+
+// alert("i am" + " " + age);
+
+// q no 2
+
+// var visit = 14;
+
+// alert("you have visited this site" + " " + visit + " " + "times");
+
+// q no 3
+
+// var birthYear = 2006;
+
+// document.write("My birth year is" + " " + birthYear + "<br>" + "Data type of my decleared variable is" + " " + typeof birthYear);
+
+// q no 4
+
+// var visitor = "John Doe" , product = "T-shirts" , quantity = 5;
+
+// document.write(visitor + " " + "odered" + " " + quantity + " " + product + " " + " on XYZ clothing store");
+
+// chapter no 4
+
+// q no 1
+
+// var name , age , qualification ;
+
+// q no 2
+
+// 5 legal variable
+
+// var firstName , last_Name , name2 , age , qualification ;
+
+// 5 illegal varaible
+
+// var 2name , first name , #age , alert , last+name ;
+
+// q no 3
+
+// document.write("Rules for naming js variables <br> Variable names should only contain . numbers $ and _ . For example : $my_1stVariable . <br>  Variables must begin with a letter, $ or _ . For example : $name , _name or name . <br> Variable names are case sensitive . <br> Variable names should not be JS Keyword .");
+
+// chapter no 5
+
+// q no 1
+
+// var num1 = 3 , num2 = 5 , sum = num1 + num2;
+
+// document.write("Sum of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + sum);
+
+// q no 2
+
+// var num1 = 3 , num2 = 5 , subtraction = num1 - num2 , multiplication = num1 * num2 , division = num1 / num2 , modulus = num1 % num2 ;
+
+// document.write("Subtraction of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + subtraction + " " + "<br>" + "Multiplication of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + multiplication + " " + "<br>" + "Division of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + division + " " + "<br>" + "Modulus of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + modulus);
+
+// q no 3
+
+// var num ;
+
+// document.write("Value after variable declaration is :" + " " + num + "<br>" );
+
+// num = 5 ;
+
+// document.write("Initial value :" + " " + num + "<br>" );
+
+// num++ ;
+
+// document.write("Value after increment is :" + " " + num + "<br>" );
+
+// var num = num + 7 ;
+
+// document.write("Value after addition is :" + " " + num + "<br>" );
+
+// num-- ;
+
+// document.write("Value after decrement is :" + " " + num + "<br>" );
+
+// var num = num % 3 ;
+
+// document.write("The remainder is :" + " " + num );
+
+// q no 4
+
+// var cost = 600 * 5;
+
+// document.write("Total cost to buy 5 tickets to a movie is" + " " + cost + " " + "PKR" );
+
+// q no 5
+
+// var num = prompt("Enter a Number") ;
+
+// document.write("Table of" + " " + num + "<br>");
+
+// for( var i = 1 ; i <= 10 ; ++ i ){
+
+//     document.write( num + " " + "*" + " " + i + " " + "=" + " " + num * i + "<br>" );
+
+// }
+
+// q no 6
+
+// var celsius = prompt("Enter temperature in Celsius");
+
+// var fahrenheit = ( celsius * 9 / 5 ) + 32 ;
+
+// document.write( celsius + " " + "Celsius" + " " + "is" + " " + fahrenheit + " " + "Fahrenheit <br>" );
+
+// var fahrenheiet = prompt("Enter temperature in Fahrenheit");
+
+// var celsius = ( fahrenheiet - 32 ) * 5 / 9 ;
+
+// document.write( fahrenheiet + " " + "Fahrenheit" + " " + "is" + " " + celsius + " " + "Celsius" );
+
+// q no 7
+
+// var item1 = 650 ;
+
+// var item2 = 100 ;
+
+// var shippingCharges = 100 ;
+
+// var quantity_1 = prompt("Quantity of item 1 is :")
+
+// var quantity_2 = prompt("Quantity of item 2 is :")
+
+// var total = ( item1 * quantity_1 ) + ( item2 * quantity_2 ) + shippingCharges ;
+
+// document.write("Price of item 1 is" + " " + item1 + "<br>" + "Quantity of item 1 is" + " " + quantity_1 + "<br>" + "Price of item 2 is" + " " + item2 + "<br>" + "Quantity of item 2 is" + " " + quantity_2 + "<br>" + "Shipping Charges is" + " " + shippingCharges + "<br>" + "Total cost of your order is" + " " + total);
+
+// q no 8
+
+// var totalMarks = prompt("Enter your Total Marks");
+
+// var obtainedMarks = prompt("Enter your Obtained Marks");
+
+// percentage = ( obtainedMarks / totalMarks ) * 100 ;
+
+// document.write("Total Marks :" + " " + totalMarks + "<br>" + "Marks Obtained :" + " " + obtainedMarks + "<br>" + "Percentage :" + " " + percentage);
+
+// q no 9
+
+// var dollar = prompt("Enter US Dollar :");
+
+// var riyal = prompt("Enter SUADI Riyal");
+
+// var rupees1 = dollar * 104.80 ;
+
+// var rupees2 = riyal * 28 ;
+
+// var total = rupees1 + rupees2 ;
+
+// document.write( dollar + " " + "US Dollar in Rupees is :" + " " + rupees1 + "<br>" + riyal + " " + "SUADI Riyal in Rupees is :" + " " + rupees2 + "<br>" + "Total currency in PKR :" + " " + total );
+
+// q no 10 
+
+// var num = 3 ;
+
+// var total = ( num + 5 * 10 ) / 2 ;
+
+// document.write(total);
+
+// q no 11
+
+// var currentYear = prompt("Enter the current Year :") , birthYear = prompt("Enter the Birth Year :") , age = currentYear - birthYear ;
+
+// document.write("Current Year :" + " " + currentYear + "<br>" + "Birth Year :" + " " + birthYear + "<br>" + "Your Age is :" + " " + age) ;
+
+// q no 12
+
+// var radius = prompt("Enter Radius :") , pie = 3.142 , circumference = 2 * pie * radius  , area = pie * (radius * radius) ;
+
+// document.write("Radius of a Circle :" + " " + radius + "<br>" + "The Circumference is :" + " " + circumference + "<br>" + "The Area is :" + " " + area)
+
+// q no 13
+
+// var snack = prompt("Enter your favorite snacke :") , currentAge = prompt("Enter your Current Age :") , maximumAge = prompt("Enter your Maximum Age :") , amount = prompt("Enter the amount of snack per day :") , total = amount * ((maximumAge - currentAge) * 365) ;
+
+// document.write("Fovorite Snack :" + " " + snack + "<br>" + "Current Age :" + " " + currentAge + "<br>" + "Estimated Maximum Age :" + " " + maximumAge + "<br>" + "Amount of Snacks per day :" + " " + amount + "<br>" + "You will need" + " " + total + " " + snack + " " + "to last you until the rip old age of" + " " + maximumAge );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
